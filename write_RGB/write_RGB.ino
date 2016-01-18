cd9121fe03932c55e94cc0802e216e9760d5f067
zhengkai1996 <zhengkai1996@hotmail.my>
2016-01-18 13:34:38 +0800

    Update write_RGB.ino

--- write_RGB/write_RGB.ino
+++ write_RGB/write_RGB.ino
@@ -1,30 +1,26 @@
-#include "reroSensor_HM.h"
-#include <SoftwareSerial.h>
-reroSensor_HM HM(2,3,8);
-void setup() {
-Serial.begin(9600);
-HM.begin(19200);
-pinMode(8,OUTPUT);
-delay(1000);// put your setup code here, to run once:
+# READ WRITE RGB () #
 
-}
+## Description ##
+This function sets head module RGB control registers  . 
 
-void loop() 
-{
+## Include ##
+reroSensor_HM.h
 
-  byte VAL=HM.writeRGB(100,New Val);
-  Serial.print("Check Head Module Infared Error:");
-  Serial.println(HM.errStatus);
-  
+## Prototype ##
+byte writeRGB(byte ID,byte colour);
 
-   if (HM.errStatus>0) { 
-    Serial.println("HM Reading Error!");
-    }
-  else
-  {
-      Serial.print(" VALUE:");
-    Serial.println(VAL);
-  }
-  delay(1000);
-  
-}
+## Parameters ##
+none
+
+## Returns ##
+Error status in byte. If return is non-zero, error occurred. Refer ReturnStatus.
+
+## Example ##
+byte writeRGB(byte 100,byte 103);
+100=(byte ID); //100 is rero head module sensor ID
+0=(byte colour); //0bit is Blue Colour
+## See Also ##
+
+InstructionSet
+
+ReturnStatus
